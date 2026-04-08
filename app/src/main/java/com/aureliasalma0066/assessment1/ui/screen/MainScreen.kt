a476e9cb5f5b783eaf8a820e8ce66e6c14e10e32
Aurelia <aureliasalma8@gmail.com>
2026-04-08 14:41:08 +0700

    HItung luas dan keliling bangun datar

--- app/src/main/java/com/aureliasalma0066/assessment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/aureliasalma0066/assessment1/ui/screen/MainScreen.kt
@@ -1,44 +1,36 @@
 package com.aureliasalma0066.assessment1.ui.screen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.aureliasalma0066.assessment1.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(navController: NavController) {
+
     var panjang by remember { mutableStateOf("") }
     var panjangError by remember { mutableStateOf(false) }
 
     var lebar by remember { mutableStateOf("") }
     var lebarError by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Smart Geometry Calculator") }
+                title = { Text("Kalkulator Bangun Datar") }
             )
         }
-
     ) { padding ->
 
         Column(
@@ -52,11 +44,20 @@ fun MainScreen(navController: NavController) {
 
             Text("Persegi Panjang", style = MaterialTheme.typography.titleLarge)
 
+            Image(
+                painter = painterResource(id = R.drawable.persegi),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth()
+            )
+
             OutlinedTextField(
-                value = "",
-                onValueChange = {},
+                value = panjang,
+                onValueChange = { panjang = it },
                 label = { Text("Panjang") },
                 isError = panjangError,
+                trailingIcon = {
+                    if (panjangError) Icon(Icons.Filled.Warning, null)
+                },
                 supportingText = {
                     if (panjangError) Text("Input tidak valid")
                 },
@@ -64,10 +65,13 @@ fun MainScreen(navController: NavController) {
             )
 
             OutlinedTextField(
-                value = "",
-                onValueChange = {},
+                value = lebar,
+                onValueChange = { lebar = it },
                 label = { Text("Lebar") },
                 isError = lebarError,
+                trailingIcon = {
+                    if (lebarError) Icon(Icons.Filled.Warning, null)
+                },
                 supportingText = {
                     if (lebarError) Text("Input tidak valid")
                 },
@@ -81,13 +85,18 @@ fun MainScreen(navController: NavController) {
 
                     if (panjangError || lebarError) return@Button
 
+                    val p = panjang.toFloat()
+                    val l = lebar.toFloat()
+
+                    val luas = p * l
+                    val keliling = 2 * (p + l)
+
+                    navController.navigate("result/$luas/$keliling")
                 },
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Text("Hitung")
             }
-
         }
     }
-
 }
